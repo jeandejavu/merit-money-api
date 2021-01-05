947b48021f8fd503ffd69958abc5bbf589cdce6d
Jean Melo <jeandejavu@hotmail.com>
2021-01-04 22:46:23 -0300

    test: infra AccountMongoRepository implements ICheckAccountByEmailRepository

--- tests/infra/db/mongodb/AccountMongoRepository.test.ts
+++ tests/infra/db/mongodb/AccountMongoRepository.test.ts
@@ -1,6 +1,7 @@
 import { AccountMongoRepository, MongoHelper } from '@/infra/db';
 import { mockAddAccountParams } from '@/tests/domain/mocks';
 import { Collection } from 'mongodb';
+import faker from 'faker';
 
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository();
@@ -28,6 +29,22 @@ describe('AccountMongoRepository', () => {
       const addAccountParams = mockAddAccountParams();
       const isValid = await sut.add(addAccountParams);
       expect(isValid).toBe(true);
+    });
+  });
+
+  describe('checkByEmail()', () => {
+    test('Should return true if email is valid', async () => {
+      const sut = makeSut();
+      const addAccountParams = mockAddAccountParams();
+      await accountCollection.insertOne(addAccountParams);
+      const exists = await sut.checkByEmail(addAccountParams.email);
+      expect(exists).toBe(true);
+    });
+
+    test('Should return false if email is not valid', async () => {
+      const sut = makeSut();
+      const exists = await sut.checkByEmail(faker.internet.email());
+      expect(exists).toBe(false);
     });
   });
 });
