95b2ab0a348a78716284a9efad5fcf753c81990a
Jean Melo <jeandejavu@hotmail.com>
2021-01-05 10:45:13 -0300

    feat: presentation protocols validation

---
+++ src/presentation/protocols/IValidation.ts
@@ -0,0 +1,4 @@
+export interface IValidation {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  validate: (input: any) => Error;
+}
--- src/presentation/protocols/index.ts
+++ src/presentation/protocols/index.ts
@@ -1,2 +1,3 @@
 export * from './IController';
 export * from './IHttpResponse';
+export * from './IValidation';
