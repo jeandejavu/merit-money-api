bb1ecb3efb1a4f91c025f447bc60464e95147b16
Jean Melo <jeandejavu@hotmail.com>
2021-01-05 10:55:53 -0300

    feat: presentation controllers SignUpController

---
+++ src/presentation/controllers/SignUpController.ts
@@ -0,0 +1,51 @@
+import {
+  IController,
+  IHttpResponse,
+  IValidation,
+} from '@/presentation/protocols';
+import {
+  badRequest,
+  serverError,
+  noContent,
+  forbidden,
+} from '@/presentation/helpers';
+import { EmailInUseError } from '@/presentation/errors';
+import { IAddAccountUseCase } from '@/domain/usecases';
+
+export class SignUpController implements IController {
+  constructor(
+    private readonly addAccount: IAddAccountUseCase,
+    private readonly validation: IValidation,
+  ) {}
+
+  async handle(request: SignUpController.Request): Promise<IHttpResponse> {
+    try {
+      const error = this.validation.validate(request);
+      if (error) {
+        return badRequest(error);
+      }
+      const { name, email, password } = request;
+      const isValid = await this.addAccount.add({
+        name,
+        email,
+        password,
+      });
+      if (!isValid) {
+        return forbidden(new EmailInUseError());
+      }
+      return noContent();
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace SignUpController {
+  export type Request = {
+    name: string;
+    email: string;
+    password: string;
+    passwordConfirmation: string;
+  };
+}
