bfe6ef068ad665ad7157d7a871497b03229a6463
Jean Melo <jeandejavu@hotmail.com>
2021-01-05 16:45:25 -0300

    feat: main config app

---
+++ src/main/config/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import setupRoutes from './routes';
+import setupMiddleware from './middlewares';
+
+const app = express();
+setupMiddleware(app);
+setupRoutes(app);
+export default app;
