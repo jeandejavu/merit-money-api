c6191945f329551e9989cebb9de274c010ee919f
Jean Melo <jeandejavu@hotmail.com>
2021-01-05 16:04:54 -0300

    feat: main add LogControllerDecoratorFactory

---
+++ src/main/factories/decorators/LogControllerDecoratorFactory.ts
@@ -0,0 +1,10 @@
+import { LogControllerDecorator } from '@/main/decorators';
+import { LogMongoRepository } from '@/infra/db';
+import { IController } from '@/presentation/protocols';
+
+export const makeLogControllerDecorator = (
+  controller: IController,
+): IController => {
+  const logMongoRepository = new LogMongoRepository();
+  return new LogControllerDecorator(controller, logMongoRepository);
+};
