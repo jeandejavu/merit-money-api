bca453f518e938e9bd3c5f7cc62a775ee6875332
Jean Melo <jeandejavu@hotmail.com>
2021-01-05 19:20:36 -0300

    feat: docs api with swagger-ui

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import setupRoutes from './routes';
 import setupMiddleware from './middlewares';
+import setupSwagger from './swagger';
 
 const app = express();
+setupSwagger(app);
 setupMiddleware(app);
 setupRoutes(app);
 export default app;
---
+++ src/main/docs/index.ts
@@ -0,0 +1,20 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Merit Money - Distribuição de pontos por merecimento',
+    description:
+      'Essa é a documentação da API feita para distribuir os pontos para pessoas que fazem a diferença !',
+    version: '1.0.0',
+    contact: {
+      name: 'Jean Melo',
+      email: 'jeanr.dejavu@gmail.com',
+      url: 'https://www.linkedin.com/in/jean-melo-00a8088a',
+    },
+  },
+  servers: [
+    {
+      url: '/api',
+      description: 'Servidor Principal',
+    },
+  ],
+};
