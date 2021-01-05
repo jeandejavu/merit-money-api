4b32280190031671a6f0967b8e41eff49dc19b57
Jean Melo <jeandejavu@hotmail.com>
2021-01-04 22:46:10 -0300

    feat: infra AccountMongoRepository implements ICheckAccountByEmailRepository

--- src/infra/db/mongodb/AccountMongoRepository.ts
+++ src/infra/db/mongodb/AccountMongoRepository.ts
@@ -1,12 +1,33 @@
 import { MongoHelper } from '@/infra/db/mongodb';
-import { IAddAccountRepository } from '@/data/protocols/db';
+import {
+  IAddAccountRepository,
+  ICheckAccountByEmailRepository,
+} from '@/data/protocols/db';
 
-export class AccountMongoRepository implements IAddAccountRepository {
+export class AccountMongoRepository
+  implements IAddAccountRepository, ICheckAccountByEmailRepository {
   async add(
     data: IAddAccountRepository.Params,
   ): Promise<IAddAccountRepository.Result> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(data);
     return result.ops[0] !== null;
+  }
+
+  async checkByEmail(
+    email: string,
+  ): Promise<ICheckAccountByEmailRepository.Result> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne(
+      {
+        email,
+      },
+      {
+        projection: {
+          _id: 1,
+        },
+      },
+    );
+    return account !== null;
   }
 }
