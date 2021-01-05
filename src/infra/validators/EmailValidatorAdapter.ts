ccc936ec688994f751a062b5f0402d0f63200c39
Jean Melo <jeandejavu@hotmail.com>
2021-01-05 13:54:45 -0300

    feat: add EmailValidatorAdapter

---
+++ src/infra/validators/EmailValidatorAdapter.ts
@@ -0,0 +1,9 @@
+import { IEmailValidator } from '@/validation/protocols';
+
+import validator from 'validator';
+
+export class EmailValidatorAdapter implements IEmailValidator {
+  isValid(email: string): boolean {
+    return validator.isEmail(email);
+  }
+}
