89fc712e43f89faceb6728adc6221a775138f523
Jean Melo <jeandejavu@hotmail.com>
2021-01-04 21:17:37 -0300

    test: data add account usecase Hasher

---
+++ tests/data/usecases/DbAddAccountUseCase.spec.ts
@@ -0,0 +1,55 @@
+import faker from 'faker';
+import { throwError } from '@/tests/utils';
+import { DbAddAccountUseCase } from '@/data/usecases';
+import { IAddAccountRepository } from '@/data/protocols/db';
+import {
+  AddAccountRepositorySpy,
+  CheckAccountByEmailRepositorySpy,
+} from '../mocks/db/account';
+import { HasherSpy } from '../mocks/cryptography';
+
+const mockAddAccountParams = (): IAddAccountRepository.Params => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+});
+
+type SutTypes = {
+  sut: DbAddAccountUseCase;
+  hasherSpy: HasherSpy;
+  addAccountRepositorySpy: AddAccountRepositorySpy;
+  checkAccountByEmailRepositorySpy: CheckAccountByEmailRepositorySpy;
+};
+
+const makeSut = (): SutTypes => {
+  const checkAccountByEmailRepositorySpy = new CheckAccountByEmailRepositorySpy();
+  const hasherSpy = new HasherSpy();
+  const addAccountRepositorySpy = new AddAccountRepositorySpy();
+  const sut = new DbAddAccountUseCase(
+    hasherSpy,
+    addAccountRepositorySpy,
+    checkAccountByEmailRepositorySpy,
+  );
+  return {
+    sut,
+    hasherSpy,
+    addAccountRepositorySpy,
+    checkAccountByEmailRepositorySpy,
+  };
+};
+
+describe('DbAddAccount Usecase', () => {
+  test('Should call Hasher with correct plaintext', async () => {
+    const { sut, hasherSpy } = makeSut();
+    const addAccountParams = mockAddAccountParams();
+    await sut.add(addAccountParams);
+    expect(hasherSpy.plaintext).toBe(addAccountParams.password);
+  });
+
+  test('Should throw if Hasher throws', async () => {
+    const { sut, hasherSpy } = makeSut();
+    jest.spyOn(hasherSpy, 'hash').mockImplementationOnce(throwError);
+    const promise = sut.add(mockAddAccountParams());
+    await expect(promise).rejects.toThrow();
+  });
+});
