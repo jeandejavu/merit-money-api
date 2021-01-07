4401dccbbeabc59b20a1c52478d7acb2f1b142f8
Jean Melo <jeandejavu@hotmail.com>
2021-01-07 19:53:18 -0300

    test: domain add field account_model in IAddAccountUseCase

--- tests/domain/mocks/MockAddAccountParams.ts
+++ tests/domain/mocks/MockAddAccountParams.ts
@@ -1,8 +1,10 @@
 import faker from 'faker';
 import { IAddAccountRepository } from '@/data/protocols/db';
+import { mockRoleModel } from './MockRoleModel';
 
 export const mockAddAccountParams = (): IAddAccountRepository.Params => ({
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
+  account_role: mockRoleModel(),
 });
