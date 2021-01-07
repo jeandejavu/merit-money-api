4691a7c7aac8e0eb907bc4334089274b31fd8275
Jean Melo <jeandejavu@hotmail.com>
2021-01-07 20:49:35 -0300

    test: add validator to check password strong

---
+++ tests/validation/validators/StrongPasswordValidator.spec.ts
@@ -0,0 +1,24 @@
+import { StrongPasswordValidator } from '@/validation/validators';
+import { InvalidStrongPasswordError } from '@/presentation/errors';
+
+import faker from 'faker';
+
+const field = faker.random.word();
+
+const makeSut = (): StrongPasswordValidator => {
+  return new StrongPasswordValidator(field);
+};
+
+describe('RequiredField Validator', () => {
+  test('Should return a MissingParamError if validation fails', () => {
+    const sut = makeSut();
+    const error = sut.validate('123456');
+    expect(error).toEqual(new InvalidStrongPasswordError());
+  });
+
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut();
+    const error = sut.validate('GKB@oUkTxRm9');
+    expect(error).toBe(true);
+  });
+});
