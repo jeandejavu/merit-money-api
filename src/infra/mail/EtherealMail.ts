29bab366ace1ef556d480ec4a469e905d42575eb
Jean Melo <jeandejavu@hotmail.com>
2021-01-29 16:22:10 -0300

    feat: change create EtherealMail

--- src/infra/mail/EtherealMail.ts
+++ src/infra/mail/EtherealMail.ts
@@ -2,36 +2,12 @@ import nodemailer, { Transporter } from 'nodemailer';
 import { IParse, ISendMail } from '@/data/protocols';
 
 export class EtherealMail implements ISendMail {
-  private client: Transporter;
-
   constructor(
     private defaultFromName: string,
     private defaultFromEmail: string,
     private mailTemplateParse: IParse,
-  ) {
-    this.getClient();
-  }
-
-  private async getClient(): Promise<Transporter> {
-    if (this.client) return this.client;
-
-    return new Promise(resolve => {
-      nodemailer.createTestAccount().then(account => {
-        const transporter = nodemailer.createTransport({
-          host: account.smtp.host,
-          port: account.smtp.port,
-          secure: account.smtp.secure,
-          auth: {
-            user: account.user,
-            pass: account.pass,
-          },
-        });
-
-        this.client = transporter;
-        resolve(this.client);
-      });
-    });
-  }
+    private transporter: Promise<Transporter>,
+  ) {}
 
   public async sendMail({
     to,
@@ -39,9 +15,9 @@ export class EtherealMail implements ISendMail {
     subject,
     templateData,
   }: ISendMail.Params): Promise<void> {
-    const client = await this.getClient();
     const html = await this.mailTemplateParse.parse(templateData);
-    const message = await client.sendMail({
+    const transporter = await this.transporter;
+    const message = await transporter.sendMail({
       from: {
         name: from?.name || this.defaultFromName,
         address: from?.email || this.defaultFromEmail,
--- src/main/factories/infra/EtherealMailFactory.ts
+++ src/main/factories/infra/EtherealMailFactory.ts
@@ -1,10 +1,22 @@
 import { EtherealMail } from '@/infra/mail';
 import { HandlebarsMailTemplate } from '@/infra/mail-template';
+import nodemailer from 'nodemailer';
 
 export const makeEtherealMailFactory = (): EtherealMail => {
   return new EtherealMail(
     'any_name',
     'any_mail@mail.com',
     new HandlebarsMailTemplate(),
+    nodemailer.createTestAccount().then(account =>
+      nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      }),
+    ),
   );
 };
